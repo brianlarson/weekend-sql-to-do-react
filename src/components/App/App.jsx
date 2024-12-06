86aec35fc176ebd850403d960ff4be97809f5117
Brian Larson <brian@brianlarson.com>
2024-12-05 22:00:43 -0600

    Finish Giphy search app-etizer before branching it, heh

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,7 @@ import { useState, useEffect } from 'react';
 import Search from '../Search/Search';
 
 function App () {
-  
-  return (
-    <div>
-      <h1>App()</h1>
-      <Search />
-    </div>
-  );
-
+  return <Search />;
 }
 
-export default App
+export default App;
