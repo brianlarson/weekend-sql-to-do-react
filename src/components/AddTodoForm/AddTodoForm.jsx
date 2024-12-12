e7a8e128c78e803270e5b97d2479b9e60b2656e0
Brian Larson <brian@brianlarson.com>
2024-12-11 22:38:56 -0600

    Initial setup for extracting AddTodoForm component

---
+++ src/components/AddTodoForm/AddTodoForm.jsx
@@ -0,0 +1,33 @@
+function AddToDoForm() {
+  return (
+    <section className="list-group list-unstyled mt-4">
+      <form onSubmit={handleSubmit} className="form mx-0">
+        <div className="row g-2">
+
+          {/* Text input */}
+          <div className="col-10">
+            <label htmlFor="todoDescription" className="visually-hidden">
+              To-do description
+            </label>
+            <input 
+              onChange={(e) => setToDoText(e.target.value)}
+              value={todoText}
+              type="text" 
+              id="todoDescription" 
+              placeholder="Add a new to-do" 
+              className="form-control" 
+            />
+          </div>
+
+          {/* Add button */}
+          <div className="col-2">
+            <button type="submit" className="btn btn-primary w-100">Add</button>
+          </div>
+
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default AddToDoForm;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import Header from '../Header/Header';
+import AddTodoForm from '../AddTodoForm/AddTodoForm';
 
 function App() {
 
@@ -165,34 +166,8 @@ function App() {
       </section>
 
       {/* Add to-do form */}
-      <section className="list-group list-unstyled mt-4">
-        <form onSubmit={handleSubmit} className="form mx-0">
-          <div className="row g-2">
-
-            {/* Text input */}
-            <div className="col-10">
-              <label htmlFor="todoDescription" className="visually-hidden">
-                To-do description
-              </label>
-              <input 
-                onChange={(e) => setToDoText(e.target.value)}
-                value={todoText}
-                type="text" 
-                id="todoDescription" 
-                placeholder="Add a new to-do" 
-                className="form-control" 
-              />
-            </div>
-
-            {/* Add button */}
-            <div className="col-2">
-              <button type="submit" className="btn btn-primary w-100">Add</button>
-            </div>
-
-          </div>
-        </form>
-      </section>
-
+      <AddTodoForm />
+      
     </div>
   );
 }
